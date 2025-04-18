1206961ce8f30d68d2ef80632b950e4334f7e8f6
Praveen Manoharan1 <praveen.manoharan1@expleogroup.com>
2025-04-18 14:24:44 +0530

    withchanges

--- src/test/java/Runner/TestCucumberRunner.java
+++ src/test/java/Runner/TestCucumberRunner.java
@@ -1,21 +1,45 @@
+//package Runner;
+//
+//import org.junit.runner.RunWith;
+//
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(tags="" ,features="src/test/features",
+//glue ="stepDefinition",
+//plugin= {
+//		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//		"pretty", "html:target/cucumber/cucumber-reports.html",
+//		"json:target/cucumber/cucumber-reports.json"
+//})
+//
+//
+//
+//
+//public class TestCucumberRunner {
+//	
+//
+//}
+
 package Runner;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags="" ,features="src/test/features",
-glue ="stepDefinition",
-plugin= {
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"pretty", "html:target/cucumber/cucumber-reports.html",
-		"json:target/cucumber/cucumber-reports.json"
-})
-
+@CucumberOptions(
+    features = "src/test/features",
+    glue = "stepDefinition",
+    plugin = {
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+        "pretty",
+        "html:target/cucumber/cucumber-reports.html",
+        "json:target/cucumber/cucumber-reports.json"
+    },
+    tags = "" // You can specify tags like "@Smoke" or leave it blank
+)
 public class TestCucumberRunner {
-	
-
+    // No need for any code inside this class
 }
-
